360c7b41889970a4ce0cb25b037df168f24f63c1
AzidoTetrazole <61944228+AzidoTetrazole@users.noreply.github.com>
2025-05-06 23:27:45 -0600

    0506252327

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "source/content/private/**"],
+    ignorePatterns: ["private", "templates", ".obsidian", "source/content/private/**", "source/content/?_*/**"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
